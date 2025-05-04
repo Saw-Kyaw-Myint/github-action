db53b6be903624b0e66f162cae576fed34fdff45
Saw Kyaw Myint <sawkyawmyint@metateammyanmar.com>
2025-05-04 22:27:18 +0630

    prevent main

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    info('tesing');
+      info('tesing');
     return $request->user();
 });
