f5055a9c5ade35f20f97cee99916ae545b549c49
Saw Kyaw Myint <sawkyawmyint@metateammyanmar.com>
2025-05-04 23:06:14 +0630

    testing pint

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    info('tesing');
+     info('tesing');
 
-    return $request->user();
+     return $request->user();
 });
